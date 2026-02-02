b0646d6e174273a1c354087b0b1c073733cbf121
Willy De Keyser <spring.boot.programming@gmail.com>
2026-02-02 14:59:31 +0100

    Init

---
+++ Spring-Boot-Security-Mustache-part14/src/main/java/willydekeyser/SpringBootSecurityMustachePart14Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityMustachePart14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityMustachePart14Application.class, args);
+	}
+
+}
